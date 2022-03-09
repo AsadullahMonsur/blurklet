824b7b622d2937d7ce1ea3c3657e6c1a46749584
AsadullahMonsur <75944119+AsadullahMonsur@users.noreply.github.com>
2022-03-09 15:37:28 +0700

    Application Properties Update
    server port and address

--- src/main/java/com/blurklet/ServletInitializer.java
+++ src/main/java/com/blurklet/ServletInitializer.java
@@ -4,7 +4,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-    //server.address = 192.168.10.21
+    //server.address=192.168.22.139
+    //server.port=4321
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BlurkletApplication.class);
